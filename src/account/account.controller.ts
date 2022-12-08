63f77f22a722f529867852e834ecdd1233c526a0
Mohsen <m.khasheii@gmail.com>
2022-12-09 00:54:23 +0330

    update account

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,20 +1,25 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/get-user.decorator';
+import { JwtGuard } from 'src/auth/jwt.guard';
+import { Roles } from 'src/auth/roles.decorators';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { userRole } from 'src/auth/user-role.enum';
 import { User } from 'src/auth/user.entity';
 import { AccountService } from './account.service';
 import { CreateAccountDto } from './dto/create-account.dto';
 
-@UseGuards(AuthGuard())
+@UseGuards(JwtGuard, RolesGuard)
 @Controller('account')
 export class AccountController {
   constructor(private accountService: AccountService) {}
 
+  @Roles(userRole.ADMIN, userRole.SUPPORT)
   @Get()
   getAllAccounts(@GetUser() user: User): Promise<User[]> {
     return this.accountService.getAllAccounts(user);
   }
 
+  @Roles(userRole.ADMIN)
   @Post()
   createAccount(
     @Body() createAccountDto: CreateAccountDto,
